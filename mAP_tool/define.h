a14ed9157d9f7414beebddcc1a61afe90046184b
doyoung930 <doyoung930@gmail.com>
2024-02-08 14:00:00 +0900

    define 수정

--- mAP_tool/define.h
+++ mAP_tool/define.h
@@ -1,8 +1,17 @@
 #pragma once
 
 struct BoundingBox {
+	float b_x;
+	float b_y;
+	float b_w;
+	float b_h;
+	float b_s;
 
+};
 
+struct PrecisionRecall {
+	float precision;
+	float recall;
 };
 
 float iou_threshold;
--- mAP_tool/mAP_calculation/DefaultMapCalculation.h
+++ mAP_tool/mAP_calculation/DefaultMapCalculation.h
@@ -8,12 +8,11 @@ class DefaultMapCalculation : public MapCalculation {
 public:
     void calculateMap(const std::vector<std::string>& data) override;
 
-    // true bbox
+    // true bboxes
     std::unordered_map<std::string, vector<struct BoundingBox>> true_bboxes;
-    // predicted bboxes,
+
+    // predicted bboxes
     std::unordered_map<std::string, vector<struct BoundingBox>> predicted_bboxes;
-
-
 
     // IoU
     std::unordered_map<std::string, float> best_IoU;
@@ -22,8 +21,8 @@ public:
         return (a > b) ? a : b;
     }
 
-
     // precision recall
+    std::unordered_map<char, vector<struct PrecisionRecall>> _precision_recall;
 
 };
 
